f2622d6f7c2ac4e6bc1882307720b324036b79e7
Ari <thearxari@gmail.com>
2024-08-11 09:21:38 +0200

    Add files via upload

---
+++ linux-run.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+
+PYTHON_SCRIPT="$SCRIPT_DIR/openshockclock.py"
+REQUIREMENTS_FILE="$SCRIPT_DIR/requirements.txt"
+VENV_DIR="$SCRIPT_DIR/venv"
+
+if [ ! -d "$VENV_DIR" ]; then
+    echo "Creating a virtual environment..."
+    python -m venv "$VENV_DIR"
+fi
+
+source "$VENV_DIR/bin/activate"
+
+if [ -f "$REQUIREMENTS_FILE" ]; then
+    echo "Installing packages from requirements.txt..."
+    pip install -r "$REQUIREMENTS_FILE"
+
+    if [ $? -ne 0 ]; then
+        echo "Failed to install packages." >&2
+        deactivate
+        exit 1
+    fi
+else
+    echo "Error: requirements.txt not found." >&2
+    deactivate
+    exit 1
+fi
+
+clear
+
+python "$PYTHON_SCRIPT"
+
+if [ $? -ne 0 ]; then
+    echo "The Python script encountered an error." >&2
+    deactivate
+    exit 1
+fi
+
+deactivate
+
