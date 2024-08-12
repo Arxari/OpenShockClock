08ab30e84e6c76fd22f9897787cf247ceb100a4e
Ari <thearxari@gmail.com>
2024-08-12 20:30:24 +0200

    Fix script not working on Debian-based distros
    changed python to python3
    
    -_-

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ VENV_DIR="$SCRIPT_DIR/venv"
 
 if [ ! -d "$VENV_DIR" ]; then
     echo "Creating a virtual environment..."
-    python -m venv "$VENV_DIR"
+    python3 -m venv "$VENV_DIR"
 fi
 
 source "$VENV_DIR/bin/activate"
@@ -39,4 +39,3 @@ if [ $? -ne 0 ]; then
 fi
 
 deactivate
-
